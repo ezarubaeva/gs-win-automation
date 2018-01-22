0ce8be9e4f8a58b23e9da166520c06f5f312be53
Elena Zarubaeva <ezarubaeva@gmail.com>
2018-01-22 19:24:33 +0300

    First commit

---
+++ src/Tools/Elem.java
@@ -0,0 +1,25 @@
+package Tools;
+
+import daima.DElement;
+import daima.KeyboardHandler;
+
+public class Elem {
+    public static DElement g(DElement _parent, String _dn, int _a, String _x, String..._xx) throws Exception{
+        return DElement.gimMeP(_parent,_dn,_a,_x,_xx);
+    }
+    public static void sleep(int _i){
+        try{
+            Thread.sleep(_i*1000);
+        } catch (Exception e){
+            System.out.println("!");
+        }
+
+    }
+
+    public static void createJob (String fileSystem) throws Exception {
+        KeyboardHandler.sendKeysHere(true,"%n");
+        KeyboardHandler.sendKeysHere(true, fileSystem);
+        KeyboardHandler.sendKeysHere(true, "{ENTER}");
+        BrowseDialog.selectLeftFolder('folder');
+    }
+}
