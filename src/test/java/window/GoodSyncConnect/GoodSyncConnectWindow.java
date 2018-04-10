812f755a071d65dcba045f73a0d396e0224d8caf
= <=>
2018-04-10 16:02:21 +0300

    Refactored GS Connect

---
+++ src/test/java/window/GoodSyncConnect/GoodSyncConnectWindow.java
@@ -0,0 +1,9 @@
+package window.GoodSyncConnectWindow;
+import Tools.Elem;
+import daima.DElement;
+
+public class GoodSyncConnectWindow extends Elem {
+    private DElement ApplyButton;
+    private DElement NextButton;
+
+}
