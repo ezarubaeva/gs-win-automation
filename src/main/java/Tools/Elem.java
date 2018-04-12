89c3a2c8f4526bcd5639a6783023c10993786934
= <=>
2018-04-12 12:17:47 +0300

    Added functions: gN, gV.

--- src/main/java/Tools/Elem.java
+++ src/main/java/Tools/Elem.java
@@ -3,6 +3,8 @@ package Tools;
 import daima.DElement;
 import daima.KeyboardHandler;
 
+import java.util.Vector;
+
 public class Elem {
     protected DElement mainWindow; // Its possible should be in Elem class
 
@@ -16,6 +18,14 @@ public class Elem {
     }
     public static DElement g(DElement _parent, String _dn, int _a, String _x, String..._xx) throws Exception{
         return DElement.gimMeP(_parent,_dn,_a,_x,_xx);
+    }
+    // should be used if we need to choose Nth element from the array of the controls
+    public static DElement gN(DElement _parent, String _dn, int _a, int _index, String _xattrlist, String..._xattrs) throws Exception{
+        return DElement.gimMePN ( _parent,_dn,_a,_index,_xattrlist,_xattrs );
+    }
+
+    public static Vector<DElement> gV(DElement _parent, String _dn, int _a, String _xattrlist, String..._xattrs) throws Exception {
+        return DElement.gimMePV ( _parent, _dn, _a , _xattrlist, _xattrs);
     }
     public static void sleep(int _i){
         try{
